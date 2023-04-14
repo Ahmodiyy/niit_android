3e5358c0c6194dd5928180ad2917c20136ef37ff
Ahmodiyy <ahmodolaitan03@gmail.com>
2023-04-14 09:50:46 +0100

    demostrating how to use intent to start an activity

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.input);
+        button.setOnClickListener((View v)-> {
+            Log.d("VIEW ID", String.valueOf(v.getId()));
+
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+    }
+}
